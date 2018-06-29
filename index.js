54cefa2f76de4fab48a57287378c6635cf6183a3
Mikhail Sinanan <mikhail.sinanan@gmail.com>
2018-06-29 06:23:53 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function dwarfRollCall(dwarves) {
+  function rollCall(dwarves){
+   var array=["Doc", "Dopey", "Bashful", "Grumpy"];
+  dwarves.forEach((dwarves,i)=>array.push(`${i+1}. ${dwarves[i]} `));
+  return array.join("Doc", "Dopey", "Bashful", "Grumpy");}
+ 
+ 
+function summonCaptainPlanet(planeteerCalls)("earth", "wind", "heart", "fire"){
+ return planeteerCalls.map(a=>a.toUpperCase()+"!");
+ }
+ 
+ function longPlaneteerCalls(words){
+  for(var i=0;i<words.length;i++){
+    if (words[i].length>4){
+      return true;
+    }
+  }
+  return false;
 }
-
-function summonCaptainPlanet(planeteerCalls){
-}
-
-function longPlaneteerCalls(words) {
-}
+ 
+var shortWords = ["wind", "fire"];
+longPlaneteerCalls(words);
 
 function findTheCheese (foods) {
+  for(var i=0;i<foods.length;i++){
+  if (foods[i]=="cheddar"
+  ||foods[i]=="gouda"
+  ||foods[i]=="camembert"
+  ){
+    return foods[i];
+  }
+}
+return "no cheese!";
 }
