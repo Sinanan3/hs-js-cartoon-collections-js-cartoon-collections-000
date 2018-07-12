731d951c99f5e5f56c42ff83b1dbb498d18e4681
Mikhail Sinanan <mikhail.sinanan@gmail.com>
2018-07-12 05:24:06 +0000

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
+
 function dwarfRollCall(dwarves){
    var string = "";
   for(var i = 0; i < dwarves.length; i++){
   string +=(`${ i+1}. ` + dwarves[i] + " " );
-}
+}//Used empty var string, test runs dwarves by name. `$ identifier to push var i + 1 to result dwarves to be listed numerically.
 return string;
   }
  
@@ -15,8 +16,8 @@ function summonCaptainPlanet(planeteerCalls){
   });
   return empty;
  }
- 
- 
+ //used empty var to push summonCaptainPlanet function through, to .toUpperCase each "element" and add "!".
+ //used forEach method to execute the summonCaptainPlane property.
  
 
 function longPlaneteerCalls(words = ["wind","fire"]){
@@ -26,9 +27,12 @@ return true;
  }
  else{
 return false; } } }
+//var i = 0 < words.length; i++ to start calling each word to see if any are greater than 4, if so, return function is set to true else it is false. 
+
+
 var snacks = ["cheddar", "pizza", "gouda", "cheezits", "camembert"];
 function findTheCheese (snacks){
-  var cheese = ["cheddar", "gouda", "camembert"];
+  var cheese = ["cheddar", "gouda", "camembert", "swiss"];
   for(var i=0; i < snacks.length; i++){
     for(var c = 0; c < cheese.length; c++){
       if(cheese[c]=== snacks[i]){
@@ -36,5 +40,21 @@ function findTheCheese (snacks){
       } 
     }
     
-     }return "no cheese!";
-}
+    }return "no cheese!"; }
+   
+   
+    function wordsWithB(words){
+    var bWords = [];
+    for(var i = 0; i < words.length; i++){
+      if (words[i].startsWith('b')){
+        bWords.push(words[i]);
+      }
+      else return "No b words!"
+      
+      //if word starts with B then push word into bWords Array
+    }
+    
+    
+    return bWords;
+    }
+     //functions findTheCheese calls the strings and runs through them to see which are cheese. used var i for var snacks to find the cheese and var c to var snacks to find the cheese(cheese.length etc.) if cheese is found return function set to cheese and return function set to "no cheese!" if no cheese found.
